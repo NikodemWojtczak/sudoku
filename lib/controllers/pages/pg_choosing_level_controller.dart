ecadb5c06de39cc9fbe4d342e2f90d7a953feaea
Nikodem Wojtczak 247823 <247823@edu.p.lodz.pl>
2023-12-12 11:44:40 +0100

    12122023

--- lib/controllers/pages/pg_choosing_level_controller.dart
+++ lib/controllers/pages/pg_choosing_level_controller.dart
@@ -1,16 +1,19 @@
 import 'package:get/get.dart';
+import 'package:sudoku/controllers/sudoku_controller.dart';
 import 'package:sudoku/pages.dart';
 
 enum Levels { hard, medium, easy }
 
 class PgChoosingLevelController extends GetxController {
   Rx<Levels> currentLevel = Levels.easy.obs;
+  SudokuController sudokuController = Get.find();
 
   onClickChangeDifficulty(Levels inputCurrentLevel) {
     currentLevel.value = inputCurrentLevel;
   }
 
   onClickLevel({required int level}) {
+    sudokuController.generateSudoku();
     Get.toNamed(Pages.pgSudokuGame, arguments: {'level': level});
   }
 }
--- lib/controllers/sudoku_controller.dart
+++ lib/controllers/sudoku_controller.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 import 'package:get/get.dart';
 
 class SudokuController extends GetxController {
-  List<int> sudokuBoard = List.generate(81, (_) => Random().nextInt(9));
+  late List<int> sudokuBoard;
   Map<int, List<int>> hints = {};
 
   @override
@@ -13,9 +13,37 @@ class SudokuController extends GetxController {
     for (int i = 0; i < 81; i++) {
       hints[i] = [];
     }
+
+    sudokuBoard = generateSudoku();
   }
 
   void setValueSudokuBoard(int index, int value) {
     sudokuBoard[index] = value;
+  }
+
+  List<int> generateSudoku() {
+    List<int> sudokuBoard = List.generate(81, (index) => 0);
+    _canBePlaced(sudokuBoard, 0, 0);
+
+    return sudokuBoard;
+  }
+
+  bool _canBePlaced(List<int> board, int index, int value) {
+    int x = index % 9;
+    int y = index ~/ 9;
+    int boxX = x ~/ 3;
+    int boxY = y ~/ 3;
+    int boxOffset = 27 * boxY + 3 * boxX;
+    for (var i = 0; i < 9; i++) {
+      int columnsValues = board[x + i * 9];
+      int rowsValues = board[y * 9 + i];
+      int boxsValues = board[boxOffset + i % 3 + 9 * (i ~/ 3)];
+      if (columnsValues == value ||
+          rowsValues == value ||
+          boxsValues == value) {
+        return false;
+      }
+    }
+    return true;
   }
 }
